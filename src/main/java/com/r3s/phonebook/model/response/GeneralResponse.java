b5e1145e031c24a48bd74a8489b298b9546786fc
rifaianwar <rifaianwar@outlook.com>
2024-07-14 00:05:50 +0700

    update

--- src/main/java/com/r3s/phonebook/model/response/GeneralResponse.java
+++ src/main/java/com/r3s/phonebook/model/response/GeneralResponse.java
@@ -3,11 +3,14 @@ package com.r3s.phonebook.model.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Setter
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GeneralResponse {
+    private String statusCode;
     private String message;
     private Object data;
 }
